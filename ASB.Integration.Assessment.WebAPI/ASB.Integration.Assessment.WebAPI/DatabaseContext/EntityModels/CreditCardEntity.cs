a587ecf94de2b79838b11c1a7550c9f2987943a7
AUCKLAND\Kamran.Muhammad <kamran.muhammad@vista.co>
2021-02-14 14:04:19 +1300

    Add entity models

---
+++ ASB.Integration.Assessment.WebAPI/ASB.Integration.Assessment.WebAPI/DatabaseContext/EntityModels/CreditCardEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASB.Integration.Assessment.WebAPI.DatabaseContext.EntityModels
+{
+    /// <summary>
+    /// Credit Card Entity.
+    /// </summary>
+    [Table("tblCreditCard")]
+    public class CreditCardEntity
+    {
+        /// <summary>
+        /// Gets or sets the Id.
+        /// </summary>
+        [Key]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the card number.
+        /// </summary>
+        public string CardNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the card expiry date.
+        /// </summary>
+        public DateTime CardExpiryDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cvc.
+        /// </summary>
+        public int Cvc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date time when it was created.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ ASB.Integration.Assessment.WebAPI/ASB.Integration.Assessment.WebAPI/DatabaseContext/EntityModels/UserLoginCredentialEntity.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ASB.Integration.Assessment.WebAPI.DatabaseContext.EntityModels
+{
+    /// <summary>
+    /// User login credential.
+    /// </summary>
+    [Table("tblUserLoginCredential")]
+    public class UserLoginCredentialEntity
+    {
+        /// <summary>
+        /// Gets or sets the Id.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user name.
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        [JsonIgnore]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        public string LastName { get; set; }
+    }
+}

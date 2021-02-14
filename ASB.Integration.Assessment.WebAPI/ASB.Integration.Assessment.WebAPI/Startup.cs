a3517ba09c307d0c239d39e3595a66c43538ac31
AUCKLAND\Kamran.Muhammad <kamran.muhammad@vista.co>
2021-02-14 14:18:23 +1300

    Add mapper to service collection

--- ASB.Integration.Assessment.WebAPI/ASB.Integration.Assessment.WebAPI/Startup.cs
+++ ASB.Integration.Assessment.WebAPI/ASB.Integration.Assessment.WebAPI/Startup.cs
@@ -1,15 +1,9 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ASB.Integration.Assessment.WebAPI
 {
@@ -25,6 +19,8 @@ namespace ASB.Integration.Assessment.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             services.AddControllers();
         }
 
